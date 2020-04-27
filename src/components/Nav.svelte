bda0fd84e4cbb6f584529b7f4364370040ff6340
John Sim <jsim@fishbowlsolutions.com>
2020-04-27 12:39:16 +0100

    update nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -25,25 +25,40 @@
 		float: left;
 	}
 
+	li a:hover {
+		color:#000;
+	}
+	li a,
 	[aria-current] {
 		position: relative;
 		display: inline-block;
 	}
-
+	li a::after {
+		position: absolute;
+		content: '';
+		width: calc(100% - 2em);
+		height: 2px;
+		background-color: #fff;
+		display: block;
+		bottom: 0px;
+		transition: background 0.2s;
+	}
+	li:hover a::after,
 	[aria-current]::after {
 		position: absolute;
 		content: '';
-		width: calc(100% - 1em);
+		width: calc(100% - 2em);
 		height: 2px;
 		background-color: #0089F7;
 		display: block;
-		bottom: -1px;
+		bottom: 0px;
 	}
 
 	a {
 		text-decoration: none;
-		padding: 1em 0.5em;
+		padding: 1em 1em;
 		display: block;
+		text-align: center
 	}
 	a.active {
 		font-weight: bold;
@@ -69,9 +84,9 @@
 <nav>
 	<ul>
 		<li><a class:active="{segment === undefined}" aria-current='{segment === undefined ? "page" : undefined}' href='.'>Overview</a></li>
-		<li><a aria-current='{segment === "solutions" ? "page" : undefined}' href='solutions'>Solutions</a></li>
-		<li><a aria-current='{segment === "pricing" ? "page" : undefined}' href='pricing'>Pricing</a></li>
-		<li><a aria-current='{segment === "demos" ? "page" : undefined}' href='demos'>Demos</a></li>
-		<li><a aria-current='{segment === "contact" ? "page" : undefined}' href='contact'>Contact Us</a></li>
+		<li><a class:active="{segment === 'solutions'}"  aria-current='{segment === "solutions" ? "page" : undefined}' href='solutions'>Solutions</a></li>
+		<li><a class:active="{segment === 'pricing'}"  aria-current='{segment === "pricing" ? "page" : undefined}' href='pricing'>Pricing</a></li>
+		<li><a class:active="{segment === 'demos'}"  aria-current='{segment === "demos" ? "page" : undefined}' href='demos'>Demos</a></li>
+		<li><a class:active="{segment === 'contact'}"  aria-current='{segment === "contact" ? "page" : undefined}' href='contact'>Contact Us</a></li>
 	</ul>
 </nav>
