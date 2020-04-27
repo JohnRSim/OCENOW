26b49bb47049a1f1d72e7a501cccf83036f939c3
John Sim <jsim@fishbowlsolutions.com>
2020-04-27 12:39:08 +0100

    update layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -30,11 +30,6 @@
 	}
 	main {
 		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
 	}
 
 	#ON-powered {
@@ -130,6 +125,9 @@
 		height: 12px;
 		margin-left: 4px;
 	}
+	.extlink {
+		display: block;
+	}
 
 	@media (max-width: 768px) {
 		header article {
@@ -151,6 +149,72 @@
 			margin-top:20px;
 		}
 
+		.extlink {
+			display: inline;
+		}
+	}
+
+	:global(.wrapper) {
+		max-width: 1000px;
+		margin:0px auto;
+	}
+
+	h2 {
+		text-align: center;
+		color:#011835;
+	}
+
+	footer {
+		background-color:#130d32;
+	}
+	.compatibility {
+		padding:20px;
+	}
+	.compatibility ul {
+		-ms-flex-wrap: wrap;
+		flex-wrap: wrap;
+		display: -ms-flexbox;
+		display: flex;
+		-ms-flex-line-pack: center;
+		align-content: center;
+		-ms-flex-pack: center;
+		justify-content: center;
+		padding: 1rem 0;
+		margin: 0 auto;
+		max-width: 40em;
+		list-style: none;
+	}
+	.compatibility ul li {
+		display: -ms-flexbox;
+		display: flex;
+		margin: 1rem;
+		padding: 0;
+		-ms-flex-align: center;
+		align-items: center;
+		font-size: 1rem;
+		font-weight: 300;
+		color: #fff;
+		opacity: .4;
+		transition: opacity .2s ease-out;
+		-webkit-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+	}
+	.compatibility ul img {
+		display: block;
+		width: 2em;
+		height: 1.5em;
+		object-fit: contain;
+		margin-right: .325em;
+	}
+	.compatibility h4 {
+		font-size: 1.25em;
+		font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif;
+		text-align: center;
+		color: #fff;
+		opacity: .4;
+		font-weight: 300;
 	}
 </style>
 
@@ -158,29 +222,42 @@
 <div id="ON-siteWrapper">
 	<HeaderNav {segment}/>
 	<header>
-		<article>
-			<h1>Your Content Experience Accelerator</h1>
-			<p>
-				Helping to rapidly create dynamic microsites, portals and support personalised users experiences with the<br /> 
-				<a class="extlink" href="https://www.oracle.com/middleware/technologies/content-experience.html" target="_blank" rel="noreferrer">Oracle Content and Experience platform</a>
-			</p>
-			<button>Learn More</button>
-		</article>
-
-		<div id="ON-powered">
-			<img id="ON-logoOracle" src="images/logo_oracle.svg" alt="Oracle" />
-			<b>POWERED BY</b>
+	
+		<div class="wrapper">
+			<article>
+				<h1>Your Content Experience Accelerator</h1>
+				<p>
+					Helping to rapidly create dynamic microsites, portals and support personalised users experiences with the 
+					<a class="extlink" href="https://www.oracle.com/middleware/technologies/content-experience.html" target="_blank" rel="noreferrer">Oracle Content and Experience platform</a>
+				</p>
+				<button>Learn More</button>
+			</article>
+			
+			<div class="wrapper" style="position:absolute;bottom:0px;width:100%;">
+				<div id="ON-powered">
+					<img id="ON-logoOracle" src="images/logo_oracle.svg" alt="Powered by Oracle" />
+					<b>POWERED BY</b>
+				</div>
+			</div>
 		</div>
 		<img class="carousel lazy" 
 			src="/images/oce_now_header_img_th.png" 
 			data-src="/images/oce_now_header_img.png" 
 			data-srcset="/images/oce_now_header_img.png 1x" 
-			alt="" />
+			alt="Oracle Content And Experience" />
 	</header>
 
-	<Nav {segment}/>
-
-	<div style="min-height:120px;background:#D4D3DE"></div>
+	<div class="wrapper">
+		<Nav {segment}/>
+	</div>
+	<div style="background:#cfe5ff; text-align:center;padding:40px;">
+		<div class="wrapper">
+			<h2>Modern Components Designed for an Enterprise User Experience</h2>
+			<p>
+				Leveraging our experience with web development to provide responsive interactive and reusable components designed for the Oracle Content and Experience platform
+			</p>
+		</div>
+	</div>
 </div>
 
 
@@ -189,3 +266,59 @@
 <main>
 	<slot></slot>
 </main>
+
+
+
+<footer>
+	<div class="wrapper compatibility">
+		<h4>Use Standalone or With Your Favorite Framework</h4>
+
+		<ul>
+						
+			<li>
+				<img src="/images/logo-react.svg" alt="" />
+				React
+			</li>
+			
+			<li>
+				<img src="/images/logo-vue.svg" alt="" />
+				Vue
+			</li>
+			
+			<li>
+				<img src="/images/logo-angular.svg" alt="" />
+				Angular
+			</li>
+			
+			<li>
+				<img src="/images/logo-svelte.svg" alt="" />
+				Svelte
+			</li>
+			
+			<li>
+				<img src="/images/logo-electron.svg" alt="" />
+				Electron
+			</li>
+
+			<li>
+				<img src="/images/logo-bootstrap.svg" alt="" />
+				Bootstrap
+			</li>
+			
+			<li>
+				<img src="/images/logo-zurb.svg" alt="" />
+				Foundation
+			</li>
+			
+			<li>
+				<img src="/images/logo-jquery.svg" alt="" />
+				jQuery
+			</li>
+			<li>
+				<img src="/images/logo-jquery.svg" alt="" />
+				JET
+			</li>
+			
+		</ul>
+	</div>
+</footer>
